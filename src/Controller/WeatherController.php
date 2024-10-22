48f1f53cabaef0c093325cc9dc01453f8427bcae
Krzysztof Piasecki <pk51070@zut.edu.pl>
2024-10-22 19:49:57 +0200

    lab-c

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Repository\LocationRepository;
+use App\Repository\MeasurementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{city}/{country?}', name: 'app_weather', requirements: ['country' => '[A-Z]{2}'])]
+    public function city(
+        string $city,
+        ?string $country = null,
+        LocationRepository $locationRepository,
+        MeasurementRepository $measurementRepository
+    ): Response {
+        if (!$country) {
+            $country = 'PL';
+        }
+
+        $location = $locationRepository->findOneBy([
+            'city' => $city,
+            'country' => $country,
+        ]);
+
+        if (!$location) {
+            throw $this->createNotFoundException('Location not found.');
+        }
+
+        $measurements = $measurementRepository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+}
